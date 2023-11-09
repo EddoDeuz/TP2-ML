d7412ad31b4294bca81854e8627cd23aceb68bf4
EddoDeuz <edgardojnx@outlook.com>
2023-11-09 14:03:56 -0300

    agregado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 let path = require("path");
-let port = 3000;
+let port = 3100;
 
 
 app.use(express.static(path.resolve(__dirname,"./public")));
@@ -15,3 +15,11 @@ app.listen(port,() => console.log(` server on port ${port}\n http://localhost:${
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+  });
+
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+  });
